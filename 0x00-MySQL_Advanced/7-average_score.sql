fec874f1461477a48eca29bfe38899f835104539
Keglo Dodzi <stephanekeglo@gmail.com>
2024-05-10 00:31:06 +0000

    Fix 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -8,12 +8,9 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT
 )
 BEGIN
-    DECLARE average FLOAT;
-
-    SET average = (SELECT AVG(score) FROM corrections 
-        WHERE user_id = user_id);
     UPDATE users
-        SET average_score = average
+        SET average_score = (SELECT AVG(score) FROM corrections 
+            WHERE corrections.user_id = user_id)
         WHERE id = user_id;
 END //
 delimiter ;
