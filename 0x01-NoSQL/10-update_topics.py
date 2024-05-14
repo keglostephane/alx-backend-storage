9095040476c3272f250d090c9deb56053bbfcc21
Keglo Dodzi <stephanekeglo@gmail.com>
2024-05-14 14:00:41 +0000

    Fix 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -10,5 +10,5 @@ def update_topics(mongo_collection, name, topics):
     :param name: school name to update
     :param topics: the list of topics in the school
     """
-    mongo_collection.update_one({'name': name},
-                                {'$set': {'topics': topics}})
+    mongo_collection.update_many({'name': name},
+                                 {'$set': {'topics': topics}})
